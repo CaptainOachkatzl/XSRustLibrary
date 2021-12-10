193f135d3d54eccbf0501a51bc6cd505f3043c38
CaptainOachkatzl <dave.hofer@hotmail.com>
2021-12-10 11:24:26 +0100

    switched to subscription based events

--- src/events.rs
+++ src/events.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 pub mod event;
 pub mod one_shot_event;
 
@@ -6,5 +8,21 @@ pub trait Invokable<T> {
 }
 
 pub trait Subscribable<T> {
-    fn subscribe(&mut self, listener: impl Fn(&T) + 'static);
+    fn subscribe(&mut self, listener: impl Fn(&T) + 'static) -> Subscription<T>;
+}
+
+pub struct Subscription<T> {
+    shared_ptr: Option<Rc<dyn Fn(&T)>>,
+}
+
+impl<T> Subscription<T> {
+    pub fn new(shared: Rc<dyn Fn(&T)>) -> Subscription<T> {
+        Subscription::<T> {
+            shared_ptr: Some(shared),
+        }
+    }
+
+    pub fn unsubscribe(&mut self) {
+        self.shared_ptr = None;
+    }
 }
