2d174bddacc159785f599ad4e2940a139f1edeca
CaptainOachkatzl <dave.hofer@hotmail.com>
2021-12-10 19:45:48 +0100

    outsourced subscription

--- src/events.rs
+++ src/events.rs
@@ -1,7 +1,8 @@
-use std::rc::Rc;
-
 pub mod event;
 pub mod one_shot_event;
+pub mod subscription;
+
+use self::subscription::Subscription;
 
 pub trait Invokable<T> {
     fn invoke(&mut self, arg: T);
@@ -11,18 +12,3 @@ pub trait Subscribable<T> {
     fn subscribe(&mut self, listener: Box<dyn Fn(&T)>) -> Subscription<T>;
 }
 
-pub struct Subscription<T> {
-    shared_ptr: Option<Rc<dyn Fn(&T)>>,
-}
-
-impl<T> Subscription<T> {
-    pub fn new(shared: Rc<dyn Fn(&T)>) -> Subscription<T> {
-        Subscription::<T> {
-            shared_ptr: Some(shared),
-        }
-    }
-
-    pub fn unsubscribe(&mut self) {
-        self.shared_ptr = None;
-    }
-}
---
+++ src/events/subscription.rs
@@ -0,0 +1,17 @@
+use std::rc::Rc;
+
+pub struct Subscription<T> {
+  shared_ptr: Option<Rc<dyn Fn(&T)>>,
+}
+
+impl<T> Subscription<T> {
+  pub fn new(shared: Rc<dyn Fn(&T)>) -> Subscription<T> {
+      Subscription::<T> {
+          shared_ptr: Some(shared),
+      }
+  }
+
+  pub fn unsubscribe(&mut self) {
+      self.shared_ptr = None;
+  }
+}
