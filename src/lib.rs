cb73341af39b5ef25f6d57e9533cf494476b9bcb
CaptainOachkatzl <dave.hofer@hotmail.com>
2021-12-10 22:54:57 +0100

    added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,45 @@
 pub mod events;
+
+#[cfg(test)]
+mod tests {
+  use std::{cell::RefCell, rc::Rc};
+
+  use crate::events::{event::Event, one_shot_event::OneShotEvent, Invokable, Subscribable};
+
+  #[test]
+  fn event_test() {
+    let counter = Rc::new(RefCell::new(0));
+    let counter_result = counter.clone();
+    let mut event = Event::<i32>::new();
+
+    let callback = move |x: &i32| {
+      let mut counter_value = counter.borrow_mut();
+      *counter_value += 1;
+      assert_eq!(*x, 3);
+    };
+
+    let _sub = event.subscribe(Box::new(callback));
+    event.invoke(3);
+
+    assert_eq!(*counter_result.borrow(), 1);
+  }
+
+  #[test]
+  fn one_shot_test() {
+    let counter = Rc::new(RefCell::new(0));
+    let counter_result = counter.clone();
+    let mut event = OneShotEvent::<i32>::new();
+
+    let callback = move |x: &i32| {
+      let mut counter_value = counter.borrow_mut();
+      *counter_value += 1;
+      assert_eq!(*x, 3);
+    };
+
+    let _sub = event.subscribe(Box::new(callback.clone()));
+    event.invoke(3);
+    let _sub = event.subscribe(Box::new(callback));
+
+    assert_eq!(*counter_result.borrow(), 2);
+  }
+}
