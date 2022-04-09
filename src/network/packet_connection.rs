ccb55ef3036501ea5b40de6e77e99f1f9f55b7d5
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-04-09 13:01:26 +0200

    evaluating and responding to TCP FIN

--- src/network/packet_connection.rs
+++ src/network/packet_connection.rs
@@ -8,6 +8,7 @@ use super::{packet_assembler::PacketAssembler, constants::{HEADER_ID_PACKET}};
 
 pub struct PacketConnection {
   tcp_stream: TcpStream,
+  shutdown_ref_stream: TcpStream,
   packet_assembler: PacketAssembler,
   receive_buffer: Vec<u8>,
 }
@@ -15,6 +16,9 @@ pub struct PacketConnection {
 impl PacketConnection {
   pub fn new(tcp_stream: TcpStream, receive_buffer_size: usize) -> PacketConnection {
     PacketConnection {
+      // copy stream to have an independently accessible object to shutdown
+      // underlying socket guarantees threadsafety
+      shutdown_ref_stream: tcp_stream.try_clone().unwrap(),
       tcp_stream,
       packet_assembler: PacketAssembler::new(),
       receive_buffer: vec![0 as u8; receive_buffer_size],
@@ -37,23 +41,28 @@ impl PacketConnection {
     Ok(())
   }
 
-  pub fn receive(&mut self) -> Result<Vec<u8>> {  
+  pub fn receive(&mut self) -> Result<Vec<u8>> {
     let mut receive_call = || {
       let size = self.tcp_stream.read(&mut self.receive_buffer)?;
       return Ok(Vec::from(&self.receive_buffer[..size]));
     };
-    return self.packet_assembler.assemble(&mut receive_call);
+
+    let shutdown_call = || {
+      return self.shutdown_ref_stream.shutdown(Shutdown::Both);
+    };
+
+    return self.packet_assembler.assemble(&mut receive_call, &shutdown_call);
   }
 
   pub fn shutdown(&self, how: Shutdown) -> Result<()> {
-    self.tcp_stream.shutdown(how)?;
+    self.shutdown_ref_stream.shutdown(how)?;
     Ok(())
   }
 
   pub fn try_clone(&self) -> Result<PacketConnection> {
-    let tcp_stream_clone = self.tcp_stream.try_clone()?;
     Ok(PacketConnection { 
-      tcp_stream: tcp_stream_clone, 
+      tcp_stream: self.tcp_stream.try_clone()?,
+      shutdown_ref_stream: self.tcp_stream.try_clone()?,
       packet_assembler: PacketAssembler::new(), 
       receive_buffer: Vec::clone(&self.receive_buffer)
     })
