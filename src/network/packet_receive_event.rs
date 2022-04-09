248e814a9cd402cb00564088c3f344a8492aea8d
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-04-09 13:03:10 +0200

    can now stop receive loop

--- src/network/packet_receive_event.rs
+++ src/network/packet_receive_event.rs
@@ -1,32 +1,86 @@
-use crate::events::{event::Event, subscription::Subscription, Subscribable, Invokable};
+use std::{
+  io::Result,
+  cell::RefCell,
+  sync::{
+    Mutex, Arc,
+  }, net::Shutdown,
+};
+
+use crate::events::{event::Event, subscription::Subscription, Invokable, Subscribable};
 
 use super::packet_connection::PacketConnection;
 
 pub struct PacketReceiveEvent {
-  packet_connection: PacketConnection,
-  receive_event: Event<Vec<u8>>,
-  started: bool,
+  packet_connection: RefCell<PacketConnection>,
+  receive_event: RefCell<Event<Vec<u8>>>,
+  started: Arc<Mutex<bool>>,
+  stop: Arc<Mutex<bool>>,
 }
 
 impl PacketReceiveEvent {
-
   pub fn new(packet_connection: PacketConnection) -> PacketReceiveEvent {
-    PacketReceiveEvent { packet_connection, receive_event: Event::new(), started: false }
+    PacketReceiveEvent {
+      packet_connection: RefCell::new(packet_connection),
+      receive_event: RefCell::new(Event::new()),
+      started: Arc::new(Mutex::new(false)),
+      stop: Arc::new(Mutex::new(false)),
+    }
   }
 
-  pub fn start(&mut self) {
-    if self.started {
+  pub fn start(&self) {
+    // guarantee only one thread can ever pass through and execute loop
+    if !self.locked_start_check() {
       return;
     }
 
-    self.started = true;
-
-    loop {
-      self.receive_event.invoke(&self.packet_connection.receive().unwrap());
+    while !*self.stop.lock().unwrap() {
+      match self.packet_connection.borrow_mut().receive() {
+        Ok(v) => self.receive_event.borrow_mut().invoke(&v),
+        _ => self.stop().unwrap(),
+      };
     }
+
+    return;
   }
 
-  pub fn subscribe(&mut self, subscriber: fn(&Vec<u8>)) -> Subscription<Vec<u8>> {
-    return self.receive_event.subscribe(subscriber);
+  fn locked_start_check(&self) -> bool {
+    return self.compare_exchange_set_bool(&self.started);
+  }
+
+  fn locked_stop_check(&self) -> bool {
+    return self.compare_exchange_set_bool(&self.stop);
+  }
+
+  // returns true if the bool was set to true
+  fn compare_exchange_set_bool(&self, bool_mutex: &Mutex<bool>) -> bool {
+    let mut lock = bool_mutex.lock().unwrap();
+    if *lock {
+      return false;
+    }
+
+    *lock = true;
+    return true;
+  }
+
+  pub fn stop(&self) -> Result<()> {
+    if !self.locked_stop_check() {
+      return Ok(());
+    }
+    
+    self.packet_connection.borrow_mut().shutdown(Shutdown::Both)?;
+    Ok(())
+  }
+
+  pub fn subscribe(&mut self, subscriber: Box<dyn Fn(&Vec<u8>) + Send + Sync + 'static>) -> Subscription<Vec<u8>> {
+    return self.receive_event.borrow_mut().subscribe(subscriber);
+  }
+
+  pub fn try_clone(&self) -> Result<PacketReceiveEvent> {
+    Ok(PacketReceiveEvent{
+      packet_connection: RefCell::new(self.packet_connection.borrow().try_clone()?),
+      receive_event: RefCell::new(Event::new()),
+      started: self.started.clone(),
+      stop: self.stop.clone(),
+    })
   }
 }
