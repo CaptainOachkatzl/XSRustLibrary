7d7f2bcdc5f251da03ff2c7edd90a78af419852f
CaptainOachkatzl <dave.hofer@hotmail.com>
2023-07-08 02:40:26 +0200

    refactor

--- src/network/packet_assembler.rs
+++ src/network/packet_assembler.rs
@@ -19,12 +19,10 @@ impl PacketAssembler {
     pub fn assemble(&mut self, receive: &mut dyn FnMut() -> Result<Vec<u8>>, shutdown: &dyn Fn() -> Result<()>) -> Result<Vec<u8>> {
         let mut packet_cursor = 0;
 
-        let mut buffer: Vec<u8>;
-        if self.leftover.is_some() {
-            buffer = self.leftover.take().unwrap();
-        } else {
-            buffer = receive()?;
-        }
+        let mut buffer = match self.leftover.take() {
+            Some(v) => v,
+            None => receive()?,
+        };
 
         if self.is_fin(&buffer) {
             shutdown()?;
@@ -39,13 +37,16 @@ impl PacketAssembler {
         while packet_cursor < packet_size {
             if !buffer.is_empty() {
                 if packet_cursor + buffer.len() > packet_size {
+                    // since too much data is availabe for the current packet, fill it up and store the unused data as leftover
                     packet.clone_from_slice(&buffer[..packet_size - packet_cursor]);
                     self.leftover = Some(buffer[packet_size - packet_cursor..].to_vec());
                     break;
                 } else {
+                    // all the data fits in the current package
                     packet[packet_cursor..packet_cursor + buffer.len()].clone_from_slice(&buffer);
                     packet_cursor += buffer.len();
 
+                    // if the available data is an exact fit for the current packet nothing else needs to be done
                     if packet_cursor == packet_size {
                         break;
                     }
