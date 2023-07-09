2be1e8ce14ab80805e02c15ea61dda70eb5d12c4
xsware <xsware@outlook.com>
2023-07-09 08:35:59 +0200

    packet assembler refactor

--- src/network/packet_assembler.rs
+++ src/network/packet_assembler.rs
@@ -1,4 +1,8 @@
-use super::{constants::HEADER_SIZE, data_buffer::DataBuffer};
+use super::{
+    constants::HEADER_SIZE,
+    data_buffer::DataBuffer,
+    packet_buffer::{PacketBuffer, PacketState},
+};
 use displaydoc::Display;
 use std::{io::Read, net::TcpStream};
 use thiserror::Error;
@@ -15,36 +19,42 @@ pub enum Error {
 
 #[derive(Clone)]
 pub struct PacketAssembler {
-    data_buffer: DataBuffer,
+    buffer: DataBuffer,
 }
 
 impl PacketAssembler {
     pub fn new(buffer_size: usize) -> PacketAssembler {
         PacketAssembler {
-            data_buffer: DataBuffer::new(buffer_size),
+            buffer: DataBuffer::new(buffer_size),
         }
     }
 
     pub fn assemble(&mut self, tcp_stream: &mut TcpStream) -> Result<Vec<u8>, Error> {
-        if self.data_buffer.is_empty() {
-            receive(tcp_stream, &mut self.data_buffer)?
-        }
-
-        if is_fin(&self.data_buffer) {
-            return Err(Error::ReceivedFin);
+        if self.buffer.is_empty() {
+            self.receive(tcp_stream)?;
         }
 
         // create a new packet
-        let packet_size = get_packet_size(&mut self.data_buffer)?;
-        let mut packet: Vec<u8> = vec![0_u8; packet_size];
+        let packet_size = get_packet_size(&mut self.buffer)?;
+        let mut packet = PacketBuffer::new(packet_size);
 
-        fill_packet(&mut packet, packet_size, &mut self.data_buffer, tcp_stream)?;
-        Ok(packet)
+        loop {
+            match packet.fill(&mut self.buffer) {
+                PacketState::Finished => return Ok(packet.into_vec()),
+                PacketState::RequiresData => self.receive(tcp_stream)?,
+            }
+        }
     }
-}
 
-fn is_fin(data: &DataBuffer) -> bool {
-    data.is_empty()
+    fn receive(&mut self, tcp_stream: &mut TcpStream) -> Result<(), Error> {
+        let size = tcp_stream.read(&mut self.buffer.get_mut_buffer())?;
+        self.buffer.set_positions(0, size);
+        if size == 0 {
+            return Err(Error::ReceivedFin);
+        }
+
+        Ok(())
+    }
 }
 
 /// read the first 4 bytes of the buffer to determine the packet size
@@ -56,37 +66,4 @@ fn get_packet_size(data: &mut DataBuffer) -> Result<usize, Error> {
 
     let size = u32::from_le_bytes(TryInto::<[u8; 4]>::try_into(header).unwrap());
     Ok(size as usize)
-}
-
-fn fill_packet(packet: &mut [u8], packet_size: usize, data: &mut DataBuffer, tcp_stream: &mut TcpStream) -> Result<(), Error> {
-    let mut packet_cursor = 0;
-    while packet_cursor < packet_size {
-        if !data.is_empty() {
-            let remaining_packet_space = packet_size - packet_cursor;
-            if data.remaining() > remaining_packet_space {
-                // since too much data is availabe for the current packet, fill it up and keep unused data in the buffer
-                packet.clone_from_slice(data.take(remaining_packet_space));
-                break;
-            } else {
-                // all the data fits in the current packet
-                let remaining_data = data.take_to_end();
-                packet[packet_cursor..packet_cursor + remaining_data.len()].clone_from_slice(remaining_data);
-                packet_cursor += remaining_data.len();
-
-                // if the available data is an exact fit for the current packet nothing else needs to be done
-                if packet_cursor == packet_size {
-                    break;
-                }
-            }
-        }
-        receive(tcp_stream, data)?;
-    }
-
-    Ok(())
-}
-
-fn receive(tcp_stream: &mut TcpStream, buffer: &mut DataBuffer) -> Result<(), Error> {
-    let size = tcp_stream.read(buffer.get_mut_buffer())?;
-    buffer.set_positions(0, size);
-    Ok(())
 }
---
+++ src/network/packet_buffer.rs
@@ -0,0 +1,55 @@
+use super::data_buffer::DataBuffer;
+
+pub enum PacketState {
+    Finished,
+    RequiresData,
+}
+
+pub struct PacketBuffer {
+    buffer: Vec<u8>,
+    current_pos: usize,
+}
+
+impl PacketBuffer {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            buffer: vec![0_u8; capacity],
+            current_pos: 0,
+        }
+    }
+
+    pub fn into_vec(self) -> Vec<u8> {
+        self.buffer
+    }
+
+    fn packet_size(&self) -> usize {
+        self.buffer.len()
+    }
+
+    fn remaining_space(&self) -> usize {
+        self.packet_size() - self.current_pos
+    }
+
+    pub fn fill(&mut self, data: &mut DataBuffer) -> PacketState {
+        if self.remaining_space() == 0 {
+            return PacketState::Finished;
+        }
+
+        let remaining_space = self.remaining_space();
+        if data.remaining() > remaining_space {
+            // since too much data is availabe for the current packet, fill it up and keep unused data in the buffer
+            self.buffer.clone_from_slice(data.take(remaining_space));
+        } else {
+            // all the data fits in the current packet
+            let remaining_data = data.take_to_end();
+            self.buffer[self.current_pos..self.current_pos + remaining_data.len()].clone_from_slice(remaining_data);
+            self.current_pos += remaining_data.len();
+
+            if self.remaining_space() > 0 {
+                return PacketState::RequiresData;
+            }
+        }
+
+        PacketState::Finished
+    }
+}
